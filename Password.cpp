c8debc1af006d984e2a88dc71a01abc6dd2ed07e
Tim Lyons <amazingasriel2001@gmail.com>
2023-04-24 11:45:08 -0700

    Submitted Gcov April24

--- Password.cpp
+++ Password.cpp
@@ -1,21 +1,46 @@
+
 #include "Password.h"
 #include <string>
 
 using std::string;
 
 /*
-  The function receives a string counts how many times the same character 
+  The function receives a string counts how many times the same character
   occurs at the beginning of the string, before any other characters (or the
   end of the string). The function is case-sensitive so 'Z' is different than
   'z' and any ASCII characters are allowed.
 */
-int Password::count_leading_characters(string phrase){
+int Password::count_leading_characters(string phrase)
+{
   int repetition = 1;
   int index = 0;
 
-  while( index < phrase.length()-1 && phrase[index] == phrase[index+1] ){
+  while (index < phrase.length() - 1 && phrase[index] == phrase[index + 1])
+  {
     repetition++;
     index++;
   }
   return repetition;
+}
+
+bool Password::has_mixed_case(string str)
+{
+  if (str == "")
+  {
+    return false;
+  }
+  bool lc = false;
+  bool uc = false;
+  for (int i = 0; i < str.size(); i++)
+  {
+    if (str[i] >= 'A' && str[i] <= 'Z')
+    {
+      uc = true;
+    }
+    if (str[i] >= 'a' && str[i] <= 'z')
+    {
+      lc = true;
+    }
+  }
+  return (lc && uc);
 }
--- PasswordTest.cpp
+++ PasswordTest.cpp
@@ -1,26 +1,64 @@
 /**
+ *
  * Unit Tests for Password class
-**/
+ **/
 
 #include <gtest/gtest.h>
 #include "Password.h"
 
 class PracticeTest : public ::testing::Test
 {
-	protected:
-		PracticeTest(){} //constructor runs before each test
-		virtual ~PracticeTest(){} //destructor cleans up after tests
-		virtual void SetUp(){} //sets up before each test (after constructor)
-		virtual void TearDown(){} //clean up after each test, (before destructor)
+protected:
+	PracticeTest() {}		   // constructor runs before each test
+	virtual ~PracticeTest() {} // destructor cleans up after tests
+	virtual void SetUp() {}	   // sets up before each test (after constructor)
+	virtual void TearDown() {} // clean up after each test, (before destructor)
 };
 
 TEST(PasswordTest, smoke_test)
 {
-    ASSERT_TRUE( 1 == 1 );
-}
+	ASSERT_TRUE(1 == 1);
+};
 TEST(PasswordTest, single_letter_password)
 {
-    Password my_password;
+	Password my_password;
 	int actual = my_password.count_leading_characters("Z");
-	ASSERT_EQ(1,actual);
-}
+	ASSERT_EQ(1, actual);
+};
+TEST(PasswordTest, lower3)
+{
+	Password my_password;
+	int actual = my_password.count_leading_characters("aaafafaq");
+	ASSERT_EQ(3, actual);
+};
+TEST(PasswordTest, upper3)
+{
+	Password my_password;
+	int actual = my_password.count_leading_characters("BBBaseB");
+	ASSERT_EQ(3, actual);
+};
+TEST(PasswordTest, mixedcase)
+{
+	Password ps;
+	EXPECT_TRUE(ps.has_mixed_case("aA"));
+};
+TEST(PasswordTest, empty)
+{
+	Password ps;
+	EXPECT_FALSE(ps.has_mixed_case(""));
+};
+TEST(PasswordTest, CaptialOnly)
+{
+	Password ps;
+	EXPECT_FALSE(ps.has_mixed_case("AA"));
+};
+TEST(PasswordTest, LowerCaseOnly)
+{
+	Password ps;
+	EXPECT_FALSE(ps.has_mixed_case("aa"));
+};
+TEST(PasswordTest, Chat10)
+{
+	Password ps;
+	EXPECT_TRUE(ps.has_mixed_case("AsfaA"));
+};
